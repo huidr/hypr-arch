a5244e4fd53a8f2d58d1e89191cfa74f8d212b91
Ronald Huidrom <mangang369@gmail.com>
2025-03-15 00:38:26 +0530

    Create config-org-mode.el

---
+++ emacs/org-mode/config-org-mode.el
@@ -0,0 +1,20 @@
+;; define header
+(defun org-header()
+  "to insert a line"
+  (interactive)
+  (insert "#+TITLE: \n")
+  (insert "#+AUTHOR: \n")
+  (insert "#+EMAIL: ronhuidrom@gmail.com")
+  (insert "#+DATE: \n")
+  (insert "#+STARTUP: overview\n")         ;; start with all headings collapsed, not "showall"
+  (insert "#+STARTUP: indent\n\n")         ;; visually indent headings for readability
+  (insert "#+OPTIONS: toc: 2\n")           ;; table of contents, level 2
+  (insert "#+LATEX_CLASS: article\n")
+  (insert "#+LATEX_CLASS_OPTIONS: [a4paper, 12pt]\n")
+  (insert "#+LATEX_HEADER: \\usepackage[margin=1.2in]{geometry}\n")
+  (insert "#+REVEAL_ROOT: https://cdn.jsdelivr.net/npm/reveal.js\n"))   ;; to use Reveal.js
+
+;; keybinding C-c h to insert header
+(with-eval-after-load 'org
+  (define-key org-mode-map (kbd "C-c h")
+    'org-header))
