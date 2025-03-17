ac83ca9717f4a6a1f3acac6910c934b7f248d54e
Ronald Huidrom <mangang369@gmail.com>
2025-03-17 23:41:37 +0530

    Create tty.conf.sh

---
+++ confs/tty.conf.sh
@@ -0,0 +1,7 @@
+# Use the following command
+# sudo systemctl edit getty@tty1
+
+[Service]
+ExecStartPre=-/usr/bin/clear
+ExecStartPre=-/usr/bin/fastfetch --config /etc/fastfetch/config.jsonc
+TTYVTDisallocate=no
